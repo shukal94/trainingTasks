328f7c3e82e0eb712ff1849f863c8302790fb459
sergey.shukalovich@gmail.com <sergey.shukalovich@gmail.com>
2016-10-10 17:33:04 +0300

    Fixed Runner

--- Runner.java
+++ Runner.java
@@ -3,31 +3,21 @@ package com.epam.training.tat.shukalovich.task06.runner;
 import com.epam.training.tat.shukalovich.task06.rules.*;
 import java.util.Scanner;
 
-/** An enter point to the application. This class realizes the business logic, such as
- * analyze the user input on according to a rules and giving an outcome of analysis to user
+/** An enter point to the application. This class realizes the 
+ * an input of a user string.
  * @version 1.0
  */
 public class Runner {
-  /** Method analyzes user input on according to a rules, such as user string contains:
-   *  1. Only numbers
-   *  2. There are no numbers in the string
-   *  3. User string contains more than 5 words
-   *  4. User string is equals to a word from dictionary
+  /** This method opens the stream for input a string by a console and runs the checking
    * @param args
    */
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     String userStr = scan.nextLine();
     
-    OnlyNumbersRule rule1 = new OnlyNumbersRule();
-    NonNumericRule rule2 = new NonNumericRule();
-    MoreThan5WordsRule rule3 = new MoreThan5WordsRule();
-    WordFromDictionaryRule rule4 = new WordFromDictionaryRule();
+    Checker check = new Checker();
     
-    System.out.println("A user string contains only numbers: " + rule1.checkRule(userStr));
-    System.out.println("A user string does not contain numbers: " + rule2.checkRule(userStr));
-    System.out.println("A user string contains more than 5 words: " + rule3.checkRule(userStr));
-    System.out.println("A user string contain a word from dictionary: " + rule4.checkRule(userStr));
+    check.runChecking(userStr);
   }
 
 }
